6d10945cf0c324b2ad7233ac75feff0530a029f1
thzy236 <576267306@qq.com>
2020-07-30 13:32:52 +0800

    Update run.cpp

--- exp5/run.cpp
+++ exp5/run.cpp
@@ -14,18 +14,22 @@ int main()
 	int inf=99999999; 
 	scanf("%d %d",&n,&m);
 	for(i=0;i<n;i++)
-		for(j=1;j<=n;j++)
+		for(j=0;j<n;j++)
 			if(i==j) e[i][j]=0;
 			else e[i][j]=inf;
 	for(i=0;i<m;i++){
 		scanf("%d %d %d",&t1,&t2,&t3);
 		e[t1][t2]=t3;
+		e[t2][t1]=t3;
 	}
-	for(k=0;k<n;k++)
-		for(i=1;i<=n;i++)
-			for(j=1;j<=n;j++)
-				if(e[i][j]>e[i][k]+e[k][j] )
-					e[i][j]=e[i][k]+e[k][j];
+	
+	for(i=0;i<n;i++)
+		for(j=0;j<n;j++)
+			for(k=0;k<n;k++)
+			if(e[i][j]>e[i][k]+e[k][j]){
+				e[i][j]=e[i][k]+e[k][j];
+				e[j][i]=e[i][k]+e[k][j];
+			}
 	for(i=0;i<n;i++){
 		for(j=0;j<n;j++){
 			printf("%10d",e[i][j]);
